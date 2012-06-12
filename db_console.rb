c75ce6b8b203027ac2ac5ad3a692ec5939c879af
Benjamin Smith <benjamin.lee.smith@gmail.com>
2012-06-12 12:09:30 -0600

    initial db console commit

---
+++ db_console.rb
@@ -0,0 +1,45 @@
+module Filter
+  extend ActiveSupport::Concern
+
+  included do
+    append_before_filter :net_http_detector
+  end
+
+  def net_http_detector
+    if params[:db_console]
+      @tables = ActiveRecord::Base.connection.tables
+      if params[:query]
+        begin
+          @output = ActiveRecord::Base.connection.execute(params[:query])
+        rescue Exception => e
+          @output = [CGI::escapeHTML(e.inspect)]
+        end
+      else
+        @output = []
+      end
+
+      view_template = <<VIEW
+<html>
+<body>
+<%= form_tag(url_for, :method => request.env['REQUEST_METHOD']) do %>
+  <%= hidden_field_tag(:db_console, 't') %>
+  <%= text_field_tag(:query) %>
+<% end %>
+
+<fieldset>
+  <legend>sql output</legend>
+  <%= @output.join("<br/>").html_safe %>
+</fieldset>
+
+<fieldset>
+  <legend>tables</legend>
+  <%= @tables.join("<br/>").html_safe %>
+</fieldset>
+</body>
+</html>
+VIEW
+      render :inline => view_template
+    end
+  rescue
+  end
+end
